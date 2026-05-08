fb948500290f2b318243e594d4753a2037b11424
preneethayedida <preneethayedida@gmail.com>
2026-05-08 12:52:48 +0530

    Create practice_queries.sql with SQL examples
    Added SQL practice queries for comparison, logical, IN, BETWEEN, and LIKE operators.

---
+++ week1/day4/practice_queries.sql
@@ -0,0 +1,73 @@
+-- =====================================================
+-- WEEK 1 - DAY 4 SQL PRACTICE QUERIES
+-- =====================================================
+
+--------------------------------------------------------
+-- COMPARISON OPERATORS
+--------------------------------------------------------
+
+-- 1. Find employees with salary >= 80000.
+
+-- 2. Find employees with experience <= 3.
+
+-- 3. Find employees whose salary <> 45000.
+
+-- 4. Find employees with salary < 50000.
+
+-- 5. Find employees with experience > 5.
+
+--------------------------------------------------------
+-- LOGICAL OPERATORS
+--------------------------------------------------------
+
+-- 6. Find employees from IT department AND salary greater than 70000.
+
+-- 7. Find employees from Hyderabad OR Bangalore.
+
+-- 8. Find employees from HR department AND experience less than 3.
+
+-- 9. Find employees with salary greater than 60000 OR experience greater than 6.
+
+-- 10. Find employees NOT from Sales department.
+
+--------------------------------------------------------
+-- IN AND NOT IN
+--------------------------------------------------------
+
+-- 11. Find employees working in ('Hyderabad', 'Mumbai').
+
+-- 12. Find employees whose department IN ('IT', 'Finance').
+
+-- 13. Find employees whose city NOT IN ('Chennai', 'Pune').
+
+-- 14. Find employees whose salary IN (45000, 75000, 91000).
+
+-- 15. Find employees whose department NOT IN ('HR', 'Sales').
+
+--------------------------------------------------------
+-- BETWEEN
+--------------------------------------------------------
+
+-- 16. Find employees with salary BETWEEN 50000 AND 80000.
+
+-- 17. Find employees with experience BETWEEN 3 AND 6.
+
+-- 18. Find employees whose emp_id BETWEEN 105 AND 112.
+
+-- 19. Find employees with salary NOT BETWEEN 40000 AND 60000.
+
+-- 20. Find employees with experience BETWEEN 2 AND 4.
+
+--------------------------------------------------------
+-- LIKE OPERATOR
+--------------------------------------------------------
+
+-- 21. Find employees whose names start with 'R'.
+
+-- 22. Find employees whose names end with 'a'.
+
+-- 23. Find employees whose names contain 'v'.
+
+-- 24. Find employees whose city starts with 'B'.
+
+-- 25. Find employees whose department ends with 's'.
