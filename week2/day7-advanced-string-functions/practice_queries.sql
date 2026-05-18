f1d4d477320a324d5522e18a1a9fc0dbe1a1d563
preneethayedida <preneethayedida@gmail.com>
2026-05-18 22:33:13 +0530

    Create practice_queries.sql for string functions
    Added practice queries for advanced string functions.

---
+++ week2/day7-advanced-string-functions/practice_queries.sql
@@ -0,0 +1,34 @@
+-- =====================================================
+-- WEEK 2 - DAY 7
+-- ADVANCED STRING FUNCTIONS PRACTICE QUERIES
+-- =====================================================
+
+-- 1. Find position of '@' in email using INSTR().
+
+-- 2. Find position of '.' in email using LOCATE().
+
+-- 3. Replace 'Data' with 'Big Data' in department names.
+
+-- 4. Reverse employee names.
+
+-- 5. Pad emp_id with zeros using LPAD().
+
+-- 6. Pad city names using RPAD().
+
+-- 7. Remove spaces from city using TRIM + REPLACE.
+
+-- 8. Display remarks using IFNULL().
+
+-- 9. Display remarks using COALESCE().
+
+-- 10. Find position of 'Analytics' using FIND_IN_SET().
+
+-- 11. Replace spaces in employee names with underscores.
+
+-- 12. Reverse city names.
+
+-- 13. Convert emails to uppercase.
+
+-- 14. Concatenate employee name and email.
+
+-- 15. Extract domain names from emails.
