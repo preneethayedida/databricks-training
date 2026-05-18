2f54c10967e26fa00cca0545d7ef91f55fb4f6bd
preneethayedida <preneethayedida@gmail.com>
2026-05-18 22:29:21 +0530

    Create practice_queries.sql for string functions
    Added practice queries for string functions.

---
+++ week2/day6-basic-string-functions/practice_queries.sql
@@ -0,0 +1,32 @@
+-- =====================================================
+-- WEEK 2 - DAY 6
+-- STRING FUNCTIONS PRACTICE QUERIES
+-- =====================================================
+
+-- 1. Find length of employee names.
+
+-- 2. Find character length of employee names.
+
+-- 3. Convert department names to uppercase.
+
+-- 4. Convert city names to lowercase.
+
+-- 5. Remove spaces using TRIM().
+
+-- 6. Remove left spaces using LTRIM().
+
+-- 7. Remove right spaces using RTRIM().
+
+-- 8. Concatenate employee name and department.
+
+-- 9. Concatenate values using CONCAT_WS().
+
+-- 10. Extract first 7 characters from email.
+
+-- 11. Extract first 5 characters using SUBSTR().
+
+-- 12. Extract characters using MID().
+
+-- 13. Extract first 4 characters using LEFT().
+
+-- 14. Extract last 3 characters using RIGHT().
