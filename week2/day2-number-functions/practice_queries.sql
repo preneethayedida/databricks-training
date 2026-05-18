c6b4726eebd046dccd549aca582be231f5eb3ac1
preneethayedida <preneethayedida@gmail.com>
2026-05-18 16:04:23 +0530

    Add basic and advanced numeric function queries

---
+++ week2/day2-number-functions/practice_queries.sql
@@ -0,0 +1,51 @@
+-- LEVEL 1 : BASIC NUMERIC FUNCTIONS
+
+-- 1. Find the absolute value of -100
+
+-- 2. Round base_salary to 0 decimal places
+
+-- 3. Round base_salary to 2 decimal places
+
+-- 4. Round up base_salary using CEIL
+
+-- 5. Round down base_salary using FLOOR
+
+
+-- LEVEL 2 : DECIMAL & NUMBER HANDLING
+
+-- 6. Truncate base_salary to 1 decimal place
+
+-- 7. Find remainder when experience_years divided by 2
+
+-- 8. Calculate power value using POWER(2,3)
+
+-- 9. Find square root of 64
+
+-- 10. Find sign of base_salary values
+
+
+-- LEVEL 3 : ADVANCED NUMERIC FUNCTIONS
+
+-- 11. Generate random number using RAND()
+
+-- 12. Format base_salary to 2 decimal places
+
+-- 13. Find greatest value between salary and bonus
+
+-- 14. Find least value between salary and bonus
+
+-- 15. Show salary after tax deduction
+
+
+-- LEVEL 4 : REAL-TIME SCENARIOS
+
+-- 16. Calculate total earnings:
+-- base_salary + bonus
+
+-- 17. Find employees with experience greater than 5 years
+
+-- 18. Calculate yearly salary
+
+-- 19. Calculate monthly salary rounded to nearest integer
+
+-- 20. Find highest salary among all employees
